a907b8940145fe3658c3df95b9a54deddabbed59
Jesuloba <jesulobajohn@gmail.com>
2021-05-14 10:36:51 +0100

    fetching user Input

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 // autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-	console.log(descriptor);
 	const originalMethod = descriptor.value;
 	const adjDescriptor: PropertyDescriptor = {
 		configurable: true,
@@ -48,10 +47,38 @@ class ProjectInput {
 		this.attach();
 	}
 
+	private gatherUserInput(): [string, string, number] | void {
+		const enteredTitle = this.titleInput.value;
+		const enteredDescription = this.descriptionInput.value;
+		const enteredPeople = this.peopleInput.value;
+
+		if (
+			enteredTitle.trim().length === 0 ||
+			enteredDescription.trim().length === 0 ||
+			enteredPeople.trim().length === 0
+		) {
+			alert("Invalid input, please try again!");
+			return;
+		} else {
+			return [enteredTitle, enteredDescription, +enteredPeople];
+		}
+	}
+
+	private clearInput() {
+		this.titleInput.value = "";
+		this.descriptionInput.value = "";
+		this.peopleInput.value = "";
+	}
+
 	@autobind
 	private submitHandler(event: Event) {
 		event.preventDefault();
-		console.log(this.titleInput.value);
+		const userInput = this.gatherUserInput();
+		if (Array.isArray(userInput)) {
+			const [title, desc, people] = userInput;
+			console.log(title, desc, people);
+		}
+		this.clearInput();
 	}
 
 	private configure() {
