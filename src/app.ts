c4e36c91871c563dfd4e4f56480e3dfd720d6d07
Jesuloba <jesulobajohn@gmail.com>
2021-05-14 09:21:27 +0100

    receiving input

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInput: HTMLInputElement;
+	descriptionInput: HTMLInputElement;
+	peopleInput: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.getElementById(
@@ -14,7 +17,29 @@ class ProjectInput {
 			true
 		);
 		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = "user-input";
+
+		this.titleInput = this.element.querySelector(
+			"#title"
+		) as HTMLInputElement;
+		this.descriptionInput = this.element.querySelector(
+			"#description"
+		) as HTMLInputElement;
+		this.peopleInput = this.element.querySelector(
+			"#people"
+		) as HTMLInputElement;
+
+		this.configure();
 		this.attach();
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInput.value);
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler.bind(this));
 	}
 
 	private attach() {
